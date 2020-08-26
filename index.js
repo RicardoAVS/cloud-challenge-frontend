ab9f1b79e244c2e81348c6044bc7236ee8f9b905
Ricardo Villada <ricardovillada@hotmail.com>
2020-08-25 19:41:46 -0500

    adding code that will call API Gateway

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
-let ones = document.getElementById('ones');
-let tenth = document.getElementById('tenth');
-let hundred = document.getElementById('hundred');
-let views = 0;
+const getJSON = async (webpage) => {
+  let url = 'https://zweaywp9rg.execute-api.us-east-1.amazonaws.com/Prod';
+  try {
+    console.log(`${url}/visitors`)
+    const response = await fetch(`${url}/visitors`, {
+      "method": 'POST',
+      "mode": 'cors',
+      "isBase64Encoded": false,
+      "headers": {
+        "Content-Type": "application/json",
+      },
+      "body": JSON.stringify({
+        Website: webpage,
+      })
+    });
 
-if (localStorage.length >= 0) {
-  for (let index = views, len = localStorage.length; index <= len; index++) {
-    let key = localStorage.key(index);
-    let value = localStorage.setItem(index, index);
-    let val = parseInt(localStorage.getItem(index));
-    views++;
-    console.log(views);
+    if (!response.ok)
+      // check if response worked (no 404 errors etc...)
+      throw new Error(response.statusText);
+
+    return response.json(); // get JSON from the response
+
+  } catch (error) {
+    return error;
   }
-  hundred.innerHTML = 0;
-  tenth.innerText = 0;
-  ones.innerText = '';
-  ones.innerText = views;
-}
+};
+
+getJSON('Cloud Challenge')
+  .then((data) => {
+    document.getElementById('hundred').innerText = String(data.count).padStart(6, "0");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
