9260b48470a1bcdf57a94a213dbe0e1dfafac13b
Ricardo Villada <ricardovillada@hotmail.com>
2020-09-10 19:54:27 -0500

    adding project to resume

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ window.onload = () => {
     let url = 'https://sr8rnh4txe.execute-api.us-east-1.amazonaws.com/Prod';
     try {
       const response = await fetch(`${url}/visitors`, {
-        "method": 'POST',
-        "mode": 'cors',
-        "isBase64Encoded": false,
-        "headers": {
+        method: 'POST',
+        mode: 'cors',
+        isBase64Encoded: false,
+        headers: {
           'Content-Type': 'application/json',
         },
-        "body": JSON.stringify({
+        body: JSON.stringify({
           Website: webpage,
         }),
       });
       return response.json(); // get JSON from the response
-
     } catch (error) {
       return error;
     }
@@ -22,19 +21,19 @@ window.onload = () => {
 
   getJSON('Cloud Challenge')
     .then((data) => {
+      let count = String(data.count);
+      let countList = count.split('');
 
-        let count = String(data.count);
-        let countList = count.split('');
+      let hundred =
+        countList[2] === undefined ? 0 : countList[0] > 0 ? countList[0] : 0;
+      let tenth =
+        countList[1] === undefined ? 0 : countList[1] > 0 ? countList[1] : 0;
+      let ones = countList[0];
 
-        let hundred = (countList[0] === undefined) ? 0 : (countList[0] > 0) ? countList[0] : 0;
-        let tenth = (countList[1] === undefined) ? 0 : (countList[1] > 0) ? countList[1] : 0;
-        let ones = countList[2]
-
-        // Show each digit from count in the corresponding box
-        document.getElementById('hundred').innerText = hundred;
-        document.getElementById('tenth').innerText = tenth
-        document.getElementById('ones').innerText = ones;
-
+      // Show each digit from count in the corresponding box
+      document.getElementById('hundred').innerText = hundred;
+      document.getElementById('tenth').innerText = tenth;
+      document.getElementById('ones').innerText = ones;
     })
     .catch((error) => {
       console.error(error);
